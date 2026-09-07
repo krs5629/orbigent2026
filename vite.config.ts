92156c081af0c0dcb13c2ff0ccff7ad81614c203
Kyathi Rose Sojan <iniziofuturo@gmail.com>
2026-09-07 17:08:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/orbigent2026/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
